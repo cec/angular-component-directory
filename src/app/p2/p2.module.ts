4ce2e0bcb1052d5550bc4a02f1bf8359759a2c8e
Francesco Rigotti <francesco.rigotti@gmail.com>
2018-04-01 23:43:29 +0200

    using ANALYZE_FOR_ENTRY_COMPONENTS

--- src/app/p2/p2.module.ts
+++ src/app/p2/p2.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {ANALYZE_FOR_ENTRY_COMPONENTS, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GammaComponent } from './gamma/gamma.component';
 import { ThetaComponent } from './theta/theta.component';
@@ -17,6 +17,9 @@ const ENTRIES = [
     ComponentDirectoryModule.forChild(ENTRIES)
   ],
   declarations: [ENTRIES.map(e => e.component)],
-  entryComponents: [ENTRIES.map(e => e.component)]
+  providers: [
+    // reuse entries instead of re-declaring entryComponents
+    {provide: ANALYZE_FOR_ENTRY_COMPONENTS, useValue: ENTRIES, multi: true}
+  ]
 })
 export class P2Module { }
